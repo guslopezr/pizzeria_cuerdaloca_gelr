6e7cdb71a692c103af8511a6d04220a592a15ff0
guslopezr <guslopezr@gmail.com>
2022-10-26 12:38:08 -0300

    correcciones al footer

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -6,7 +6,7 @@ function Footer() {
     <div className='footer-container'>
       <h3 className='footer-title'>Gustavo López</h3>
       <div className='footer-links'>
-        <a href="https://github.com/guslopezr" target="_blank">Github</a><span></span>
+        <a href="https://github.com/guslopezr/pizzeria_cuerdaloca_gelr" target="_blank">Link a repositorio en Github para este proyecto</a><span></span>
 
         </div>
     </div>
